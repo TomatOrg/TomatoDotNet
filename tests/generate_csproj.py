bca5e2edb7e53958ee91d4c639a318a84e8f4364
Itay Almog <itay2828@gmail.com>
2025-11-02 00:06:51 +0200

    cleanup the test project generation script, added more tests

--- tests/generate_csproj.py
+++ tests/generate_csproj.py
@@ -1,9 +1,8 @@
 import shlex
-import shutil
-import tempfile
+from pathlib import Path
 import os
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+CURRENT_DIR = Path(__file__).resolve().parent
 
 CSPROJ_TEMPLATE = """
 <Project Sdk="Microsoft.NET.Sdk">
@@ -24,37 +23,47 @@ CSPROJ_TEMPLATE = """
 </Project>
 """
 
+def get_cs_from_csproj(f: Path):
+    return f.read_text().split('Compile Include="', 1)[1].split('"', 1)[0]
 
-projects = []
-
-
-def generate_csproj(path, depth=1):
-    ticks = "\\".join(['..'] * depth)
-
-    for filename in os.listdir(path):
-        full_path = os.path.join(path, filename)
-
-        if os.path.isdir(full_path):
-            if filename in ['bin', 'obj']:
+def iter_tests(path: Path, suffix: str):
+    for value in path.iterdir():
+        if value.is_dir():
+            if value.name in ['bin', 'obj']:
                 continue
-            generate_csproj(full_path, depth + 1)
-            continue
+            yield from iter_tests(value, suffix)
+        elif value.is_file(follow_symlinks=False):
+            if value.suffix == suffix:
+                yield value
 
-        if not filename.endswith(".cs"):
-            continue
+EXISTING_PROJECTS: set[str] = set()
+EXISTING_SOURCES: set[Path] = set()
+PROJECTS: list[Path] = []
 
-        print('Generating {}proj'.format(os.path.relpath(full_path, CURRENT_DIR)))
+# start by creating the existing mappings
+for csproj in iter_tests(CURRENT_DIR, suffix='.csproj'):
+    cs = csproj.parent / get_cs_from_csproj(csproj)
+    EXISTING_SOURCES.add(cs)
+    EXISTING_PROJECTS.add(csproj.name.removesuffix('.csproj'))
+    PROJECTS.append(csproj)
 
-        projects.append(os.path.relpath(full_path, CURRENT_DIR) + 'proj')
+for cs in iter_tests(CURRENT_DIR, suffix='.cs'):
+    if cs in EXISTING_SOURCES:
+        continue
 
-        # create a project file
-        with open(full_path + 'proj', 'w') as f:
-            f.write(CSPROJ_TEMPLATE.format(filename, ticks))
+    # ensure the project name is unique
+    csproj_name = cs.name.removesuffix('.cs').lower()
+    while csproj_name in EXISTING_PROJECTS:
+        csproj_name += '_'
+    EXISTING_PROJECTS.add(csproj_name)
 
-# generate all the csprojects
-generate_csproj(CURRENT_DIR)
+    dashes = str(CURRENT_DIR.relative_to(cs, walk_up=True)).replace("/", '\\')
+    csproj = cs.parent / (csproj_name + '.csproj')
+    print(f"Generating {csproj.relative_to(CURRENT_DIR)}")
+    csproj.write_text(CSPROJ_TEMPLATE.format(cs.name, dashes))
+    PROJECTS.append(csproj)
 
 # build the solution
-if not os.path.exists(os.path.join(CURRENT_DIR, 'tests.sln')):
+if not (CURRENT_DIR / 'tests.sln').exists():
     assert os.system(f'cd {CURRENT_DIR} && dotnet new sln') == 0
-assert os.system(f'cd {CURRENT_DIR} && dotnet sln add {shlex.join(projects)}') == 0
+assert os.system(f'cd {CURRENT_DIR} && dotnet sln add {shlex.join([str(proj) for proj in PROJECTS])}') == 0
